90d4fc747c3185d4b92f9e8e44774cbcd01bbe65
valentinamcabrera <vcabrera@correo.unimet.edu.ve>
2026-02-28 18:51:33 -0400

    main, esta con JPanel en vez de JFrame

--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -4,6 +4,8 @@
  */
 package proyecto1;
 
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author valen
@@ -14,7 +16,29 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        java.awt.EventQueue.invokeLater(() -> {
+           
+            javax.swing.JFrame ventana = new javax.swing.JFrame("Proyecto 1 - Analizador de Proteínas");
+            
+            // 2. Le decimos que al darle a la 'X', el programa se cierre de verdad
+            ventana.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+            
+            // 3. Creamos tu panel (tu diseño)
+            Interfaz miPanel = new Interfaz();
+            
+            // 4. Metemos tu diseño dentro de la ventana
+            ventana.setContentPane(miPanel);
+            
+            // 5. Ajustamos el tamaño de la ventana al tamaño de tu diseño
+            ventana.pack();
+            
+            // 6. La centramos en la pantalla
+            ventana.setLocationRelativeTo(null);
+            
+            // 7. ¡Hágase la luz!
+            ventana.setVisible(true);
+        });
     
+    
+    }
 }
